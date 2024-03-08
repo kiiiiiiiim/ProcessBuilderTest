f57a7c76404e64eca432cefe4b4ed5b8105b1d37
kiiiiiiiis <edgar.kim@sdt.inc>
2024-03-08 07:47:06 +0000

    Initial commit

---
+++ src/main/java/com/example/processbuildertest/ProcessBuilderTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.processbuildertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessBuilderTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessBuilderTestApplication.class, args);
+    }
+
+}
